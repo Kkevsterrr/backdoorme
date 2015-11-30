ef1689e70121350b49fada03e1222e0fa82a4428
Kevin Bock <koolkevin@gbfamily.com>
2015-11-30 17:42:42 -0500

    fixed assert in tests

--- tests.py
+++ tests.py
@@ -2,6 +2,6 @@ from imports import *
 from os import *
 
 def add_target_test():
-    assertEqual(True, True)
+    pass
     #subprocess.call("python master.py")
     #assertEqual(bd.localIP, "10.1.0.1")
