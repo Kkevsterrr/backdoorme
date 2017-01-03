172f35a9810b93cebffe5bea4d0b4b0e9dff5f39
Kkevsterrr <rstudio@example.com>
2017-01-02 23:20:23 -0500

    testing here we come

--- stats.py
+++ stats.py
@@ -3,41 +3,66 @@ from nose.tools import nottest
 import pexpect
 import random
 import sys
+from containers import *
 
-NUM_TESTS = 1
-try:
-    IP = sys.argv[1]
-    USERNAME = sys.argv[2]
-    PASS = sys.argv[3]
-except:
-    IP = "192.168.121.153"
-    USERNAME = "george"
-    PASS = "password" 
-    print("Usage is: python stats.py <IP> <USERNAME> <PASSWORD>, but defaulting to %s@%s:%s" %(USERNAME, IP, PASS))
+NUM_TESTS = 5
+PASSRATE = 0.9
+DOCKER = False
 
+@nottest
+def setup():
+    if DOCKER:
+        machines = {}
+        client = docker.from_env()
+        create_archive()
+
+        print(GOOD + "Creating attacker...")
+        machines["Attacker"] = Machine()
+        print(GOOD + "Setting up attacker...")
+        setup_attacker(machines["Attacker"])
+
+        print(GOOD + "Creating target...")
+        machines["Target"] = Machine()
+        print(GOOD + "Setting up target...") 
+        setup_target(machines["Target"])     
+        machines["Attacker"].run("python dependencies.py")
+        print(machines["Target"].get_ip()) 
+        return machines, machines["Target"].get_ip(), "george", "password"
+    
+    else:
+        try:
+            return {}, sys.argv[1], sys.argv[2], sys.argv[3]
+        except:  
+            return {}, "192.168.121.153", "george", "password" 
+    
 @nottest
 def get_port():
     return random.randrange(1024, 65535, 1)
 
 @nottest
 def testAddTarget():
-    print "Spawning..."
-    child = pexpect.spawn('python master.py')
+    #if not DOCKER:
+    #    child = pexpect.spawn('python master.py')
+    #else: 
+        #attacker = None  # TODO
+    child = pexpect.spawn("docker exec -it b8f0a8a60bb2 python master.py")# % attacker.docker_id)
+    print("Spawned.")
     child.expect('Using local IP')
     child.sendline('addtarget')
     child.expect('Target Hostname:')
     child.sendline(IP)
-    print "Getting there"
     child.expect('Username:')
     child.sendline(USERNAME)
     child.expect('Password:')
     child.sendline(PASS)
     child.expect('Target')
+    print("Opening connection...")
     child.sendline('open')
     child.expect('Connection established.')
-    print "wooohooo"
+    print("Connection established...")
     return child
 
+@nottest
 def test_pyth():
     child = testAddTarget()
     child.sendline('use shell/pyth')
@@ -53,6 +78,7 @@ def test_pyth():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_perl():
     child = testAddTarget()
     child.sendline('use shell/perl')
@@ -67,6 +93,7 @@ def test_perl():
     child.sendline('whoami')
     child.expect('root', timeout=10)
 
+@nottest
 def test_bash():
     child = testAddTarget()
     child.sendline('use shell/bash')
@@ -82,6 +109,7 @@ def test_bash():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_sh():
     child = testAddTarget()
     child.sendline('use shell/sh')
@@ -97,6 +125,7 @@ def test_sh():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_sh2():
     child = testAddTarget()
     child.sendline('use shell/sh2')
@@ -112,6 +141,7 @@ def test_sh2():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_bash2():
     child = testAddTarget()
     child.sendline('use shell/bash2')
@@ -127,6 +157,7 @@ def test_bash2():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_x86():
     child = testAddTarget()
     child.sendline('use shell/x86')
@@ -142,6 +173,7 @@ def test_x86():
     child.expect('root', timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_nc():
     child = testAddTarget()
     child.sendline('use shell/netcat')
@@ -157,6 +189,7 @@ def test_nc():
     child.expect(USERNAME, timeout=10)
     #print("weve got rood")
 
+@nottest
 def test_php():
     child = testAddTarget()
     child.sendline('use shell/php')
@@ -171,16 +204,34 @@ def test_php():
     child.sendline('whoami')
     child.expect('root', timeout=10)
 
-tests = {"Python" : test_pyth, "Perl" : test_perl, "Bash" : test_bash, "Bash2" : test_bash2, "Sh" : test_sh, "Sh2" : test_sh2, "Netcat" : test_nc, "x86" : test_x86, "PHP" : test_php } 
-
-for test in tests:
+def check(test):
     success = 0
     for num in range(1, NUM_TESTS + 1):
         try:
-            tests[test]()
+            test()
             success += 1
-        except:
+        except Exception as e:
+            print(e)
             pass
-    print "-------------"
-    print "%s final: %d/%d" %(test, success, num)
-    print "-------------"
+    print("-------------")
+    print("%s final: %d/%d" %(test, success, num))
+    print("-------------")
+    if success < PASSRATE * NUM_TESTS:
+        assert False
+    else:
+        pass
+
+#machines, IP, USERNAME, PASS = setup()
+IP = "172.17.0.3"
+USERNAME = "george"
+PASS = "password"
+def test_all():
+    try:
+        #tests = {"Python" : test_pyth, "Perl" : test_perl, "Bash" : test_bash, "Bash2" : test_bash2, "Sh" : test_sh, "Sh2" : test_sh2, "Netcat" : test_nc, "x86" : test_x86, "PHP" : test_php } 
+        tests = {"Bash" : test_bash}
+        for test in tests:
+            yield check, tests[test] 
+    finally:
+        print(GOOD + "Cleaning up...")
+        for m in machines:
+            machines[m].stop()
